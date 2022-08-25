0c6d1ddbd42ba61436f9a27a6fabdc860ddb651f
MRo47 <rodriguesmyron47@gmail.com>
2022-08-26 00:11:22 +0100

    updated node main

--- src/genicam_camera_node_main.cc
+++ src/genicam_camera_node_main.cc
@@ -31,19 +31,17 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <ros/ros.h>
-#include <nodelet/loader.h>
+#include <rclcpp/rclcpp.hpp>
+#include "genicam_camera_node.h"
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "rc_genicam_camera");
+  rclcpp::init(argc, argv);
+  auto genicam_camera_node = std::make_shared<rcgccam::GenICamCameraNode>(
+    "rc_genicam_camera_node");
 
-  nodelet::Loader manager(false);
-  nodelet::M_string remappings(ros::names::getRemappings());
-  nodelet::V_string my_argv(argv + 1, argv + argc);
+  rclcpp::spin(genicam_camera_node);
 
-  manager.load(ros::this_node::getName(), "rc_genicam_camera", remappings, my_argv);
-
-  ros::spin();
+  rclcpp::shutdown();
   return 0;
 }
